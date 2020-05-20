3e719b8c21492a12ec17f639feb10766a956b4e9
li-caspar <li-caspar@hotmail.com>
2020-05-20 17:52:14 +0800

    重写

--- week05/server.js
+++ week05/server.js
@@ -1,10 +1,20 @@
-const http = require("http")
+const http = require('http');
 
 const server = http.createServer((req, res) => {
+  //打印request的headers
+  console.log(req.headers);
+
+  var postData = "";//post提交上来的数据
+  req.on("data", function(chunk){
+    postData += chunk;
+  });
+  req.on("end", function(){
+    console.log('postData', postData);
+  })
+
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('X-Foo', 'bar');
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
   res.end('ok');
 });
-
-server.listen(8080)
+server.listen(8088);
